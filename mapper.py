f47eed90f2c500307acfe417d8f349dc48bb2e11
Thomas-Xiang <xiang38@wisc.edu>
2021-12-08 21:50:29 -0600

    max'

--- mapper.py
+++ mapper.py
@@ -5,8 +5,14 @@ import sys
 def mapper():
     for line in sys.stdin:
         data = line.strip().split(",")
-        timestamp, city, item, cost = data
-        print(city + "," + cost)
+        try:
+            timestamp, city, item, cost = data
+            print(city + "," + cost)
+        except:
+            pass
+
+
+
 
 if __name__ == "__main__":
     # what function should run when python mapper.py is called?
